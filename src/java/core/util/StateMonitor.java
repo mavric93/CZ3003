de6a01f58c36133608598c9766c7e05631441cb1
NCT-PC\n <nct_124@hotmail.com>
2017-04-04 15:29:05 +0800

    changed the notify agency part in console.html,added the skeleton for Sending email to PM office

--- src/java/core/util/StateMonitor.java
+++ src/java/core/util/StateMonitor.java
@@ -17,10 +17,13 @@ import javax.swing.Timer;
 public class StateMonitor extends TimerTask {
 
     private boolean currentCrisisState;
-    CrisisDAO crisisDAO = new CrisisDAO();
-
+    CrisisDAO crisisDAO ;
+	Calendar start,end;
+	
+	
     public StateMonitor(boolean isCrisis) {
         this.currentCrisisState = isCrisis;
+		crisisDAO = new CrisisDAO();
         System.out.println("started");
     }
 
@@ -28,15 +31,28 @@ public class StateMonitor extends TimerTask {
     public void run() {
         boolean newCrisisState = crisisDAO.checkComfirmedCrisis();
         boolean stateChanged = newCrisisState && this.currentCrisisState;
-
-        if (currentCrisisState && !newCrisisState) {
+		
+		if (!currentCrisisState && newCrisisState) {//from non-crisisState to crisisState
+			start = Calendar.getInstance();
+		}
+        if (currentCrisisState && !newCrisisState) {//from crisisState to non-crisisState
             //send final report
+			end = Calendar.getInstance();
             System.out.println("send last report " + System.nanoTime());
+			ReportController con = new ReportController();
+			JSONArray reportContent = con.listResolved(start,end);
+			
+			//after sending final report
+			start = null;
+			end = null;
         }
+		
 
         currentCrisisState = newCrisisState;
         if (currentCrisisState) {
             System.out.println("send email " + System.nanoTime());
+			ReportController con = new ReportController();
+			JSONArray reportContent = con.listOngoing();
         } else {
             System.out.println("no ongoing crisis new message " + System.nanoTime());
 
